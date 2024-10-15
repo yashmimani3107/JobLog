f51f2636b3b86225258e039079787f256b57b954
Yash Mimani <yashmimani3107@gmail.com>
2024-10-15 10:47:29 +0530

    craeted JobFrom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import JobForm from './components/JobForm';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-200 p-4 shadow-lg">
       <h1 className='text-3xl font-bold text-center mb-8'>JobLog : Your career, your progress—tracked effortlessly. </h1>
+      <JobForm/>
     </div>
   );
 }
