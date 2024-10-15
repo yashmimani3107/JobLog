d143befe962fe664c81f5a89124ace63bc149588
Yash Mimani <yashmimani3107@gmail.com>
2024-10-15 10:04:41 +0530

    initial commit setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-200 p-4 shadow-lg">
+      <h1 className='text-3xl font-bold text-center mb-8'>JobLog : Your career, your progress—tracked effortlessly. </h1>
     </div>
   );
 }
